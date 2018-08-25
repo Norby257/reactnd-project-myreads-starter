1a0ed587823a9b073219effbfbab3373df6435a8
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-08-25 13:33:25 -0500

    initial searchBooks component

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import './App.css'
 import CurrentlyReading from './CurrentlyReading';
 import WantToRead from './WantToRead';
 import Read from './Read';
+import SearchBooks from './SearchBooks';
 
 class BooksApp extends React.Component {
+ 
   state = {
     /**
      * TODO: Instead of using this state variable to keep track of which page
@@ -17,29 +19,12 @@ class BooksApp extends React.Component {
   }
 
   render() {
+    
     return (
       <div className="app">
         {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
+          
               <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
         ) : (
           <div className="list-books">
             <div className="list-books-title">
@@ -47,11 +32,10 @@ class BooksApp extends React.Component {
             </div>
             <div className="list-books-content">
               <div>
+              <SearchBooks />
                 <CurrentlyReading />
                 <WantToRead />
                 <Read />
-                
-                
               </div>
             </div>
             <div className="open-search">
