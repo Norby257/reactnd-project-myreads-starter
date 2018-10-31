e38cdcf0b8b499bf1d93fff72fc7d1c549a2b6c2
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-10-31 17:57:11 -0500

    added debounce; refactor book into class component

--- src/Book.js
+++ src/Book.js
@@ -1,8 +1,9 @@
 import React from 'react';
+
 //   TODO - pass slect as a prop to book on the shelf changer div 
 // write a method and then pass that too as a prop 
 import Select from './Select'
-function Book(props) {
+class Book extends React.Component {
 
 //   updateShelf = (event) => {
 //     this.setState({selectValue: event.target.value})
@@ -10,31 +11,35 @@ function Book(props) {
 // }
     //   TODO: update code so it checks to see if thumbnail exists and if not, return a default no image avail 
     //   TODO: user needs to be able to move a book between the shelves 
-        return (
-            <li className="book-list-item">
-            <div className="book">
-              <div className="book-top">
-              <div
-                className="book-cover"
-                style={{
-                  width: 128,
-                  height: 193,
-                  backgroundImage: `url(${props.books.imageLinks.smallThumbnail ? props.books.imageLinks.smallThumbnail : null})`
-                  
-                }}
-              />
-
-<Select />
-
-              </div>
-           
-              <div className="book-title">{props.books.title}</div>
-              <div className="book-authors"> {props.books.authors ? props.books.authors.join(', ') :' '} </div>
-              <span> {props.books.publishedDate} </span>
+    render() {
+      return (
+        <li className="book-list-item">
+        <div className="book">
+          <div className="book-top">
+          <div
+            className="book-cover"
+            style={{
+              width: 128,
+              height: 193,
+              backgroundImage: `url(${this.props.books.imageLinks.smallThumbnail ? this.props.books.imageLinks.smallThumbnail : null})`
               
-            </div>
-          </li>
-        )
+            }}
+          />
+
+              <Select />
+
+          </div>
+       
+          <div className="book-title">{this.props.books.title}</div>
+          <div className="book-authors"> {this.props.books.authors ? this.props.books.authors.join(', ') :' '} </div>
+          <span> {this.props.books.publishedDate} </span>
+          
+        </div>
+      </li>
+    )
+
+  }
+  
     
 
 }
--- src/Select.js
+++ src/Select.js
@@ -1,59 +1,50 @@
-import React from 'react'; 
+import React from "react"
 
 class Select extends React.Component {
-    // TODO: state here 
-    state = {
-        shelf: 'none'
-    }
+  // TODO: state here
+  state = {
+    shelf: "none"
+  }
 
-    //  TODO: functions here - ok so this had the right idea, just re-naming it 
+  //  TODO: functions here - ok so this had the right idea, just re-naming it
 
-    // updateShelf = (event) => {
-    //     this.setState({selectValue: event.target.value})
-    //     console.log(this.state)
-    // }
-    
-        //  get book by book id 
-        //  push that book to array
-        //  where state name is equal to the value selected?
+  updateShelf = (event) => {
+      this.setState({selectValue: event.target.value})
+      console.log(this.state)
+  }
 
-        // BooksAPI.update()
-        // .then((book)=>{
+  //  get book by book id
+  //  push that book to array
+  //  where state name is equal to the value selected?
 
-        // })
-       
+  // BooksAPI.update()
+  // .then((book)=>{
 
-        //  push to array? not sure where exactly that is stored
-        
-       
+  // })
 
-       
+  //  push to array? not sure where exactly that is stored
 
+  //  TODO render here
+  render() {
+    const { event } = this.state
 
-    //  TODO render here 
-    render() {
-        const {event} = this.state
-       
-        return (
-            <div className="book-shelf-changer" onChange={(event) => this.updateShelf(event.target.value)}>
-            
-            <select value={this.state.selectValue} onChange ={this.handleChange}>
-            <option value="move" disabled>
-              Move to...
-            </option>
-            <option value="currentlyReading">
-              Currently Reading
-            </option>
-            <option value="wantToRead">Want to Read</option>
-            <option value="read">Read</option>
-            <option value="none">None</option>
-          </select>
-          </div>
-
-        )
-    }
-
-
+    return (
+      <div
+        className="book-shelf-changer"
+        onChange={event => this.updateShelf(event.target.value)}
+      >
+        <select value={this.state.selectValue} onChange={this.handleChange}>
+          <option value="move" disabled>
+            Move to...
+          </option>
+          <option value="currentlyReading">Currently Reading</option>
+          <option value="wantToRead">Want to Read</option>
+          <option value="read">Read</option>
+          <option value="none">None</option>
+        </select>
+      </div>
+    )
+  }
 }
 
-export default Select 
+export default Select
